5aa52991824776ddd31b2112f2da359bf1d25a46
Q <isakkovicq@gmail.com>
2025-03-01 20:23:12 +0100

    Routes

--- src/tg-shop/hoc/ShopLayout.tsx
+++ src/tg-shop/hoc/ShopLayout.tsx
@@ -96,6 +96,22 @@ const ShopLayout: React.FC<ShopLayoutProps> = ({ children }) => {
               <span
                 className="cursor-pointer"
                 onClick={() => {
+                  navigate(generalRoutes.MY_COURSES);
+                }}
+              >
+                {t("mycourses")}
+              </span>
+              <span
+                className="cursor-pointer"
+                onClick={() => {
+                  navigate(generalRoutes.MY_PROFILE);
+                }}
+              >
+                {t("myprofile")}
+              </span>
+              <span
+                className="cursor-pointer"
+                onClick={() => {
                   navigate(generalRoutes.COURSES);
                 }}
               >
