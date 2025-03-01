8beb4ac48414918d462269a788565c712f560ade
Q <isakkovicq@gmail.com>
2025-03-01 14:38:11 +0100

    cart

--- src/tg-shop/components/Cart.tsx
+++ src/tg-shop/components/Cart.tsx
@@ -1,5 +1,5 @@
 import CloseIcon from "@assets/svgs/icons/CloseIcon";
-import { useEffect, useRef, useMemo } from "react";
+import { useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { coursesOverviewItems } from "../../mock";
 import DeleteIcon from "@assets/svgs/icons/DeleteIcon";
